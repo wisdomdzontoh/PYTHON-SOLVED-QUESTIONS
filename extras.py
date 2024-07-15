854f8c420e916b8ae858952f966b8bed6558781b
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-07-15 00:02:15 +0000

    rock paper scissors game added

---
+++ extras.py
@@ -0,0 +1,13 @@
+"""ROCK, PAPER, SCISSORS GAME"""
+
+import random
+
+computer_guess = ["rock", "paper", "scissors"]
+user_guess = input("Enter your choice: rock, paper, scissors: ")
+computer_guess = random.choice(computer_guess)
+
+
+
+#computer_guess = random.choice(computer_guess)
+print(computer_guess)
+
